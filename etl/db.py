a3addd385591fa8657d8f1eaf56db74aea51d426
Tingdong Chen <chentingdong@gmail.com>
2024-09-03 13:58:36 -0700

    garmin data scripts and config

--- etl/db.py
+++ etl/db.py
@@ -1,43 +1,17 @@
 import os
 import json
 import logging
-import sqlite3
 import psycopg2
 
-def save_activities_to_sqlite(activities):
-    conn = sqlite3.connect('/Users/tingdong/projects/trainme/sqlite/strava_activities.db')
-    cursor = conn.cursor()
-
-    for activity in activities:
-        try:
-            cursor.execute('''
-                INSERT INTO activities (id, name, distance, moving_time, type, start_date)
-                VALUES (?, ?, ?, ?, ?, ?)
-            ''', (
-                activity['id'],
-                activity['name'],
-                activity['distance'],
-                activity['moving_time'],
-                activity['type'],
-                activity['start_date']
-            ))
-            conn.commit()
-        except Exception as e:
-            logging.error('Error: %s', e)
-            conn.rollback()
-            break
-
-    conn.close()
+conn = psycopg2.connect(
+    dbname='trainme',
+    user='postgres',
+    password='docker',
+    host='localhost',
+    port=os.getenv('POSTGRES_PORT', 5432)
+)
 
 def save_activities_to_postgres(activities):
-    conn = psycopg2.connect(
-        dbname='trainme',
-        user='postgres',
-        password='docker',
-        host='localhost',
-        port=os.getenv('POSTGRES_PORT', 5432)
-    )
-    
     cursor = conn.cursor()
     
     for activity in activities:
@@ -66,16 +40,32 @@ def save_activities_to_postgres(activities):
             break
         
     conn.close()
-    
-def flatten_dict(d, parent_key='', sep='.'):
-    items = []
-    for k, v in d.items():
-        new_key = f"{parent_key}{sep}{k}" if parent_key else k
-        if isinstance(v, dict):
-            items.extend(flatten_dict(v, new_key, sep=sep).items())
-        else:
-            items.append((new_key, v))
-    return dict(items)
 
-def flatten_activity(activity):
-    return flatten_dict(activity)
+def save_laps_to_postgres(laps):
+    cursor = conn.cursor()
+    for lap in laps:
+        try:
+            # Convert lists and dicts to JSON strings
+            for key, value in lap.items():
+                if isinstance(value, (list, dict)):
+                    lap[key] = json.dumps(value)
+            
+            escaped_columns = [f'"{col}"' if '.' in col else col for col in lap.keys()]
+            columns = ', '.join(escaped_columns)
+            placeholders = ', '.join(['%s' for _ in lap])
+            update_clause = ', '.join([f'{col} = EXCLUDED.{col}' for col in lap.keys()])
+            
+            sql = f'''
+                INSERT INTO laps ({columns}) 
+                VALUES ({placeholders}) 
+                ON CONFLICT (id)
+                DO UPDATE SET {update_clause}
+            '''
+            
+            cursor.execute(sql, list(lap.values()))
+            conn.commit()
+        except Exception as e:
+            logging.error('Error: %s', e)
+            conn.rollback()
+            break
+    cursor.close()
---
+++ etl/strava_laps.py
@@ -0,0 +1,73 @@
+# Download all strava activity laps. Only need to successfully run it once.
+# Idenpodent operation.
+
+import os
+import logging
+import requests
+import time
+from etl.db import save_laps_to_postgres, conn
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# get the client_id and client_secret from the environment variables
+client_id = os.getenv('STRAVA_CLIENT_ID')
+client_secret = os.getenv('STRAVA_CLIENT_SECRET')
+
+# Temporary access token taken from browser session storage.
+ACCESS_TOKEN = 'dd6cea1ca89da73b1a2761e9d486cfcfae8df733'
+
+url = 'https://www.strava.com/api/v3/activities/{id}/laps'
+
+def fetch_strava_laps(activity_id) -> list:
+    """_summary_
+    fetch strava laps from the API for a given activity id
+    """
+
+    logging.info("Fetching laps for activity %s", activity_id);
+    response = requests.get(url.format(id=activity_id),
+        headers={'Authorization': f'Bearer {ACCESS_TOKEN}'},
+        timeout=60
+    )
+
+    if response.status_code != 200:
+        print(f"Error: {response.status_code} - {response.text}")
+        return []
+
+    return response.json()
+
+
+def sync_all_strava_laps():
+    """_summary_
+    fetch all laps from strava.
+    """
+
+    try:
+        # get all activities from the database
+        cursor = conn.cursor()
+        cursor.execute('''
+            SELECT id 
+            FROM activities a
+            WHERE NOT EXISTS (
+                SELECT 1 
+                FROM laps l 
+                WHERE (l.activity->>'id')::text = a.id::text
+            )
+        ''')
+        result = cursor.fetchall()
+        activity_ids = [int(row[0]) for row in result]
+        cursor.close()
+
+        logging.info("Found %s activities without laps", len(activity_ids))
+        
+        for activity_id in activity_ids:
+            fetched_laps = fetch_strava_laps(activity_id)
+            if not fetched_laps or len(fetched_laps) == 0:
+                continue
+            save_laps_to_postgres(fetched_laps)
+            time.sleep(1)
+    except KeyboardInterrupt:
+        logging.info("Process interrupted by user. Exiting gracefully...")
+    finally:
+        logging.info("Fetched laps for %s activities from Strava.", len(activity_ids))
+        
+if __name__ == '__main__':
+    sync_all_strava_laps()
