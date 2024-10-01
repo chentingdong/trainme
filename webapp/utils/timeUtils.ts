644b1850ade4df8bf3be4e4f593bc2b1ba79caa4
Tingdong Chen <chentingdong@gmail.com>
2024-09-30 19:54:33 -0700

    week start on Monday

--- webapp/utils/timeUtils.ts
+++ webapp/utils/timeUtils.ts
@@ -1,6 +1,7 @@
 import { intervalToDuration } from "date-fns";
 import { startOfWeek, addDays } from "date-fns";
 
+
 export const formatTimeSeconds = (seconds: number): string => {
   const duration = intervalToDuration({ start: 0, end: seconds * 1000 });
   const { hours = 0, minutes = 0, seconds: secs = 0 } = duration;
@@ -23,7 +24,7 @@ export const formatTimeSeconds = (seconds: number): string => {
 export const getCurrentWeek = (aday?: Date): Date[] => {
   // today is as good as any.
   if (!aday) aday = new Date();
-  const start = startOfWeek(aday);
+  const start = startOfWeek(aday, { weekStartsOn: 1 });
 
   return Array.from({ length: 7 }, (_, i) => addDays(start, i));
 };
