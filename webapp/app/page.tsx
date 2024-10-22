697428fcdd1b2146a579707adcca1cfa50e77ba3
Tingdong Chen <chentingdong@gmail.com>
2024-10-21 19:31:02 -0700

    clean up

--- webapp/app/page.tsx
+++ webapp/app/page.tsx
@@ -12,7 +12,7 @@ const Page = () => {
     >
       <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
         <h1 className="text-white text-4xl md:text-6xl font-bold text-center px-4">
-          Plan B is a compromise of plan A
+          Plan B is a compromise of Plan A.
         </h1>
       </div>
     </div>
