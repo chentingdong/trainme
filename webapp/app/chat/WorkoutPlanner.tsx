31d1b3bcbe74b708076e17da653de5eb0e2bfa6a
Tingdong Chen <chentingdong@gmail.com>
2024-10-29 13:51:11 -0700

    clean up

--- webapp/app/chat/WorkoutPlanner.tsx
+++ webapp/app/chat/WorkoutPlanner.tsx
@@ -26,13 +26,26 @@ export const WorkoutPlanner: React.FC<{ message: Message }> = (props) => {
     <div className='chat-output'>
       <div className='chat-response font-ai'>{chatResponse}</div>
       {workouts && workouts.length > 0 && (
-        <ul className='workouts grid grid-cols-3 gap-4 my-4'>
+        <ul className='workouts my-4'>
           {workouts.map((workout: Workout, index: number) => (
-            <li key={index} className='col-span-1'>
+            <li key={index} className='col-span-1 w-full grid grid-cols-6 mb-4'>
+              <div className='col-span-1 flex flex-col gap-1'>
+                <div>{new Date(workout.date).toLocaleDateString()}</div>
+                <div className='text-xs'>
+                  {new Date(workout.date).toLocaleString('default', {
+                    weekday: 'long',
+                  })}
+                </div>
+              </div>
+              <div className='col-span-1 flex flex-col gap-1'>
+                <div>{workout.duration}</div>
+                <div>{workout.distance}</div>
+              </div>
               <div
                 className={cn([
                   'card cursor-pointer flex-shrink-0 shadow-md rounded-lg',
                   'text-2xs tracking-tight cursor-pointer',
+                  'col-span-3',
                 ])}
                 onClick={() => workout && setEditorWorkout(workout)}
               >
