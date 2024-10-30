c283daced816fc285baf3b35479203b49b2430cd
Tingdong Chen <chentingdong@gmail.com>
2024-10-30 15:29:05 -0700

    fix debugging with vscode

--- db/src/index.ts
+++ db/src/index.ts
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 export * from '@prisma/client';
 
-const LOG_LEVELS: Prisma.LogLevel[] = ['info', 'warn', 'error'];
+const LOG_LEVELS: Prisma.LogLevel[] = ['warn', 'error'];
 
 const getPrismaClient = () => {
   if (process.env.VERCEL_ENV) {
